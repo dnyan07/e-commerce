d614b90e5ebe9a1318b9c029d771c01609b2f764
Dnyaneshwar Koli <dnyaneshwarkoli@MacBook-Pro.local>
2024-05-30 23:23:28 +0530

    about us page

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -3,7 +3,21 @@ import React from 'react'
 function About() {
   return (
     <div>
-      <h1 className='text-2xl font-bold text-stone-900 underline flex justify-center'>About Us </h1>
+      <h1 className='text-2xl font-bold text-stone-900 underline flex justify-center'>Contact Us </h1>
+      <div className='flex justify-center m-4 p-4'>
+       <form >
+        <label>FirstName : 
+          <input className="bg-stone-500 border-stone-800 text-stone-200 m-2 p-2" type='text'/>
+        </label>
+        <label>LastName : 
+          <input className="bg-stone-500 border-stone-800 text-stone-200 m-2 p-2" type='text'/>
+        </label>
+        <label>Email : 
+          <input className="bg-stone-500 border-stone-800 text-stone-200 m-2 p-2" type='text'/>
+        </label>
+        <button className='bg-green-500 m-2 p-2 text-black font-bold rounded'>Submit</button>
+       </form>
+      </div>
     </div>
   )
 }
